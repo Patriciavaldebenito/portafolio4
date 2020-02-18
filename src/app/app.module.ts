630beec31b43001c1ef609a5691ccd3646032fdf
Patriciavaldebenito <patricia.valdebenito@outlook.cl>
2020-02-18 20:37:15 -0300

    distribucion espacion, componente home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 
-
-
 /* AGREGADO  */
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+/*import { NgbdCarouselPause } from './carousel-pause';*/
+
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
@@ -19,23 +22,30 @@ import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
+
   declarations: [
     AppComponent,
+
     HomeComponent,
+
     MenuComponent,
     AboutComponent,
     ProjectsComponent,
     SkillComponent,
-    ContactComponent
+    ContactComponent,
+
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    NgbModule,
     MDBBootstrapModule.forRoot()
   ],
   providers: [],
+
   bootstrap: [AppComponent],
   schemas:[NO_ERRORS_SCHEMA]
+  
 })
 export class AppModule { }
